e86c93c21dafc521bfe7d4a1d8623bb55aea2213
32byte <xlebedenko@gmail.com>
2021-11-02 21:09:13 +0100

    Argument parsing + better logging

--- src/bin.rs
+++ src/bin.rs
@@ -1,20 +1,50 @@
 extern crate compressor;
 
-use std::fs;
+use clap::{App, Arg};
 use compressor::*;
+use std::fs;
 
 pub fn main() {
-    // TODO: create a tui / arg parsing
-    // TODO: better logging / statistics
-    let content = fs::read_to_string("data/input.svg").expect("File not found!");
+    let matches = App::new("svgcompressor")
+        .version("0.1.0")
+        .author("Alexey Lebedenko")
+        .about("Merges neighboring elements of the same kind into one")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file to compress")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Output file to save to")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("iterations")
+                .short("i")
+                .long("iter")
+                .takes_value(true)
+                .help("Ammount of iterations to compress")
+                .default_value("8"),
+        )
+        .get_matches();
+
+    let iterations = match matches.value_of("iterations").unwrap().parse::<usize>() {
+        Ok(iter) => iter,
+        Err(_) => panic!("Interations have to be a positive natural number!"),
+    };
+
+    let content = fs::read_to_string(matches.value_of("INPUT").unwrap()).expect("File not found!");
 
     let mut compressed = content;
-
-    for i in 0..8 {
-        println!("Iteration {}/8", i);
+    // TODO: better logging / statistics
+    for i in 0..iterations {
+        println!("Iteration {}/{}", i, iterations);
         compressed = compress(&compressed);
     }
 
-    fs::write("data/output.svg", compressed).expect("Couldn't write to file!");
+    fs::write(matches.value_of("OUTPUT").unwrap(), compressed).expect("Couldn't write to file!");
     println!("Done!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ extern crate roxmltree;
 
 use roxmltree::{Attribute, Node};
 use std::collections::HashMap;
-use std::mem;
 use std::io::Write;
+use std::mem;
 
 /*
 WARNING/TODOs:
@@ -85,83 +85,6 @@ impl Distance {
         Distance(integer_decode(val))
     }
 }
-
-/*
-pub fn nts(node: &Node) -> String {
-    // ignores empty lines and comments
-    if !node.is_element() && !node.is_text() {
-        return "".to_string();
-    }
-
-    if node.is_text() {
-        // roxmltree parses spaces between elements as text, which can be ignored
-        if node
-            .text()
-            .unwrap()
-            .to_string()
-            .replace(" ", "")
-            .replace("\n", "")
-            == ""
-        {
-            return "".to_string();
-        }
-
-        return node.text().unwrap().to_string();
-    }
-
-    /*
-    if node doesn't have children
-    <node key="value">node_value</node>
-    */
-    let node_name = node.tag_name().name();
-
-    // parse children
-    let mut children = "".to_string();
-    if node.has_children() {
-        for e in node.children() {
-            children += &nts(&e);
-        }
-    }
-
-    // parse attributes
-    let mut attributes = " ".to_string();
-    for attr in node.attributes() {
-        attributes += &format!("{}=\"{}\" ", attr.name(), attr.value());
-    }
-    // cut off last attributes char since it's a space
-    attributes = attributes[0..attributes.len() - 1].to_string();
-
-    let mut namespace = "".to_string();
-    if node_name == "svg" {
-        namespace = format!(
-            " xmlns=\"{}\"",
-            node.tag_name().namespace().unwrap().to_string()
-        );
-    }
-
-    /* Note: To make the xml more clean this can be implemented
-        I am not sure though if this might cause problems,
-        so it's not implemented right now.
-
-    if children == "" {
-        return format!(
-            "<{node}{attributes}{namespace} />",
-            node = node_name,
-            attributes = attributes,
-            namespace = namespace
-        );
-    }
-    */
-
-    format!(
-        "<{node}{attributes}{namespace}>{children}</{node}>",
-        node = node_name,
-        attributes = attributes,
-        namespace = namespace,
-        children = children
-    )
-}
-*/
 
 pub fn merge_rects(a: &Node, b: &Node) -> String {
     // merge children
@@ -254,7 +177,12 @@ pub fn children_to_string(node: &Node) -> String {
 
             counter += 1;
             if node.tag_name().name() == "svg" {
-                print!("\r{}/{} {}/{}", counter, all_ele_len, (counter as i64 - other_children.len() as i64).max(0), other_children.len());
+                print!(
+                    "\r{}/{} rects, {:.2}% total",
+                    counter,
+                    rect_children.len(),
+                    counter as f64 / all_ele_len as f64 * 100_f64
+                );
                 std::io::stdout().flush().unwrap();
             }
 
@@ -332,7 +260,12 @@ pub fn children_to_string(node: &Node) -> String {
         for e in &other_children {
             counter += 1;
             if node.tag_name().name() == "svg" {
-                print!("\r{}/{} {}/{}", counter, all_ele_len, (counter as i64 - other_children.len() as i64).max(0), other_children.len());
+                print!(
+                    "\r{}/{} rects, {:.2}% total",
+                    counter,
+                    rect_children.len(),
+                    counter as f64 / all_ele_len as f64 * 100_f64
+                );
                 std::io::stdout().flush().unwrap();
             }
 
@@ -419,197 +352,3 @@ pub fn compress(content: &str) -> String {
 
     compress_to_string(&doc.root().first_child().unwrap())
 }
-/*
-pub fn node_to_string(node: &roxmltree::Node) -> String {
-    let mut attr_string = String::new();
-    for attr in node.attributes() {
-        attr_string += &format!("{}='{}' ", attr.name(), attr.value());
-    }
-    // TODO: clean up this scuffed mess
-    if node.tag_name().name() == "text" {
-        format!(
-            "<{} {}>{}</{}>",
-            node.tag_name().name(),
-            &attr_string[0..attr_string.len() - 1],
-            node.text().unwrap(),
-            node.tag_name().name()
-        )
-    } else if node.tag_name().name() == "svg" {
-        format!(
-            "<{} {} xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink'>",
-            node.tag_name().name(),
-            format!("width='{}' height='{}'", node.attribute("width").unwrap(), node.attribute("height").unwrap())
-        )
-    } else {
-        format!("<{} {}/>", node.tag_name().name(), attr_string)
-    }
-}
-
-pub fn compress(input_path: &str, output_path: &str) {
-    let content = fs::read_to_string(input_path).expect("File not found!");
-
-    let doc = roxmltree::Document::parse(&content).expect("Couldn't parse svg");
-    let mut output = String::new();
-
-    // Keys for the equality check
-    let keys: Vec<String> = vec!["fill".to_string()];
-
-    let mut children: Vec<Node> = Vec::new();
-    let mut elem_blacklist: Vec<Node> = Vec::new();
-    let mut x_map: HashMap<Distance, Vec<Node>> = HashMap::new();
-    let mut y_map: HashMap<Distance, Vec<Node>> = HashMap::new();
-    let mut non_rect_nodes: Vec<Node> = Vec::new();
-
-    // Populate the x & y maps and children
-    for e in doc.root().first_child().unwrap().children() {
-        if !e.is_element() {
-            continue;
-        }
-
-        if e.tag_name().name() != "rect" {
-            non_rect_nodes.push(e);
-            continue;
-        }
-
-        match x_map.get_mut(&Distance::new(e.get_x())) {
-            Some(v) => v.push(e),
-            None => {
-                x_map.insert(Distance::new(e.get_x()), vec![e]);
-            }
-        }
-
-        match y_map.get_mut(&Distance::new(e.get_y())) {
-            Some(v) => v.push(e),
-            None => {
-                y_map.insert(Distance::new(e.get_y()), vec![e]);
-            }
-        }
-
-        children.push(e);
-    }
-
-    // Sort children by x and y
-    children.sort_by(|a, b| {
-        if a.get_y() < b.get_y() {
-            return std::cmp::Ordering::Less;
-        }
-        if a.get_y() == b.get_y() {
-            return a.get_x().partial_cmp(&b.get_x()).unwrap();
-        }
-        return std::cmp::Ordering::Greater;
-    });
-
-    for c in &mut children {
-        if elem_blacklist.contains(c) {
-            continue;
-        }
-
-        // check if there is element to the right
-        // Note: Children have to be sorted since checking for neighbors to the left is impossible
-        match x_map.get_mut(&Distance::new(c.get_x() + c.get_w())) {
-            Some(neighbors) => {
-                for e in neighbors {
-                    if elem_blacklist.contains(e) {
-                        continue;
-                    }
-
-                    if c.get_y() == e.get_y() && c.same(&e, &keys) {
-                        // TODO: merge
-
-                        // "remove" e & c
-                        elem_blacklist.push(*e);
-                        elem_blacklist.push(*c);
-
-                        break;
-                    }
-                }
-            }
-            None => (),
-        }
-    }
-
-    //let mut rect: Option<roxmltree::Node> = None;
-
-    output += &node_to_string(&doc.root().first_child().unwrap());
-    output += "\n";
-
-    // Pseudo-code
-    /*
-    let x_map = hashmap(children, key='x');
-    let y_map = hashmap(children, key='y');
-
-    for c in children {
-        // check to the right
-        for e in x_map[c.x+c.w] {
-            if e.y == c.y {
-                merge(e, c)
-                x_map.remove(e)
-                break;
-            }
-        }
-
-        // check to the bottom
-        for e in y_map[c.y+c.h] {
-            if e.x == c.x {
-                merge(e, c)
-                y_map.remove(e)
-                break;
-            }
-        }
-    }
-    */
-
-    // Assumes there are no nested tags
-    /*
-    for elem in doc.root().first_child().unwrap().children() {
-        if !elem.is_element() {
-            continue;
-        }
-
-        // Lets for first only compress rectangles
-        if elem.tag_name().name() == "rect" {
-            if rect.is_none() {
-                rect = Some(elem);
-                continue;
-            } else {
-                // Rect is some, try to connect rect and elem
-                let rx: f32 = rect.unwrap().attribute("x").unwrap().parse().unwrap();
-                let ry: f32 = rect.unwrap().attribute("y").unwrap().parse().unwrap();
-                let rw: f32 = rect.unwrap().attribute("width").unwrap().parse().unwrap();
-                let rh: f32 = rect.unwrap().attribute("height").unwrap().parse().unwrap();
-
-                let ex: f32 = elem.attribute("x").unwrap().parse().unwrap();
-                let ey: f32 = elem.attribute("y").unwrap().parse().unwrap();
-                let ew: f32 = elem.attribute("width").unwrap().parse().unwrap();
-                let eh: f32 = elem.attribute("height").unwrap().parse().unwrap();
-
-                // Above or below each other
-                if rx == ex {
-                    println!("Above or below");
-                    // rect above elem
-                    if ry + rh == ey + 1. {
-                        println!("rect above elem");
-                        rect = None;
-                    }
-                } else {
-                    println!("{} {}", rx, ex)
-                }
-            }
-        }
-        output += &node_to_string(&elem);
-        output += "\n";
-    }
-    */
-    output += "</svg>";
-
-    let elem = doc
-        .descendants()
-        .find(|n| n.tag_name().name() == "rect")
-        .unwrap();
-
-    println!("{:?}", &elem);
-    println!("{}", node_to_string(&elem));
-
-    fs::write(output_path, output).expect("Couldn't write to file!");
-}
- */
